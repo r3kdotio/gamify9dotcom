f3f0aa6b4f1849a9a94883f84f61654c2812902a
Richard Kolb <richardk@hiliosits.com>
2017-08-25 18:34:59 +0200

    uniform next

---
+++ src/main/frontend/src/app/challenge2/gameenginedesktopjpct.component.ts
@@ -0,0 +1,66 @@
+import { BaseChallengeComponent } from '../shared/basechallenge.component';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+
+import { Challenge2Service } from './challenge2.service';
+
+@Component({
+  template: `
+<div class="animated fadeIn">
+        <div class="card">
+            <div class="card-header">
+        Let's add the automatic module "requires jpct;" to the module-info.java.<br/>
+        This should now compile with a single warning.<br/>
+            </div>
+            <div class="card-body">
+                <prism-hack-block [code]="moduleinfo" [language]="'java'"></prism-hack-block>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                compile again
+            </div>
+            <div class="card-body">
+                <prism-hack-block [code]="compile" [language]="'java'"></prism-hack-block>
+            </div>
+        </div>
+
+        <div>
+            <textarea #input class="form-control" rows="3"></textarea>
+            <button (click)="submit(input.value)" class="btn btn-primary">Submit</button>
+        </div>
+    <button (click)="next()" *ngIf="success" type="button" class="btn btn-success">Success, next challenge</button>
+    <div *ngIf="errorMessage" class="alert alert-danger">
+      <strong>Error!</strong>{{errorMessage}}
+    </div>
+
+
+ </div>`
+})
+export class GameEngineDesktopJPCTComponent extends BaseChallengeComponent {
+
+  moduleinfo =
+  `module com.gamify9.gameengine {
+
+  exports com.gamify9.gameengine;
+  requires java.desktop;
+  requires jpct;
+
+}`
+
+  compile = 'javac --module-path 3rdparty/jpct-1.jar com.gamify9.gameengine/com/gamify9/gameengine/score/GameScore.java com.gamify9.gameengine/com/gamify9/gameengine/GameEngine.java com.gamify9.gameengine/module-info.java  -d mods/com.gamify9.gameengine'
+
+
+  constructor(private challenge1Service: Challenge2Service, private router: Router) {
+    super();
+  }
+
+  submit(input) {
+    console.info("Submit " + input);
+    this.challenge1Service.readInputWithModuleInfoDesktopAndJPCT(input).subscribe(res => this.evaluateResponse(res), error => { this.error(error) });
+  }
+  next() {
+    this.router.navigate(['./challenge2/5']);
+  }
+}
